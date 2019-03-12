cb950a2fa99be7f07c5b0d7967138fc9fde5f7e7
Purevdalai <purewdalai3@gmail.com>
2019-03-12 17:19:08 +0800

    Complete usage camera

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,198 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { File, FileReader, FileEntry } from '@ionic-native/file/ngx';
+import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/camera/ngx';
+import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
+import { ActionSheetController, ToastController, Platform, LoadingController } from '@ionic/angular';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 
+import { finalize } from 'rxjs/operators';
+
+const STORAGE_KEY = 'my_images';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+  images = [];
+
+  constructor(
+    private camera: Camera, private file: File, private http: HttpClient,
+    private webView: WebView, private storage: Storage,
+    private actionSheetController: ActionSheetController, private plt: Platform,
+    private toastController: ToastController, private ref: ChangeDetectorRef,
+    private loadingController: LoadingController
+  ) { }
+
+  ngOnInit() {
+    this.plt.ready().then(() => {
+      this.loadStoredImages();
+    });
+  }
+
+  loadStoredImages() {
+    this.storage.get(STORAGE_KEY).then(images => {
+      if ( images ) {
+        const arr = JSON.parse(images);
+        this.images = [];
+        for ( const img of arr ) {
+          const filePath = this.file.dataDirectory + img;
+          const resPath = this.pathForImage(filePath);
+          this.images.push({ name: img, path: resPath, filePath: filePath });
+        }
+      }
+    });
+  }
+
+  pathForImage(img) {
+    if ( img === null ) {
+      return '';
+    } else {
+      const converted = this.webView.convertFileSrc(img);
+      return converted;
+    }
+  }
+
+  async presentToast(text) {
+    const toast = await this.toastController.create({
+      message: text,
+      position: 'bottom',
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  async selectImage() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Select Image source',
+      buttons: [{
+        text: 'Load from library',
+        handler: () => {
+          this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+        }
+      }, {
+        text: 'Use Camera',
+        handler: () => {
+          this.takePicture(this.camera.PictureSourceType.CAMERA);
+        }
+      }, {
+        text: 'Cancel',
+        role: 'cancel'
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  takePicture(sourceType: PictureSourceType) {
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: sourceType,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+
+    this.camera.getPicture(options).then(imagePath => {
+      const currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+      const correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+      this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+    });
+  }
+  createFileName() {
+    const date = new Date(), n = date.getTime(), newFileName = n + '.jpg';
+    return newFileName;
+
+  }
+
+  copyFileToLocalDir(namePath, currentName, newFileName) {
+    this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName)
+    .then(_ => {
+      this.updateStoredImages(newFileName);
+    }, error => {
+      this.presentToast('Error while storing file');
+    });
+  }
+
+  updateStoredImages(name) {
+    this.storage.get(STORAGE_KEY).then(images => {
+      const arr = JSON.parse(images);
+      if ( !arr ) {
+        const newImages = [name];
+        this.storage.set(STORAGE_KEY, JSON.stringify(newImages));
+      } else {
+        arr.push(name);
+        this.storage.set(STORAGE_KEY, JSON.stringify(arr));
+      }
+
+      const filePath = this.file.dataDirectory + name;
+      const resPath = this.pathForImage(filePath);
+
+      const newEntry = {
+        name: name,
+        path: resPath,
+        filePath: filePath
+      };
+
+      this.images = [ newEntry, ...this.images ];
+      this.ref.detectChanges();
+    });
+  }
+
+  deleteImages(imgEntry, position) {
+    this.images.splice(position, 1);
+
+    this.storage.get(STORAGE_KEY).then(images => {
+      const arr = JSON.parse(images);
+      const filtered = arr.filter(name => name !== imgEntry.name);
+      this.storage.set(STORAGE_KEY, JSON.stringify(filtered));
+      const correctPath = imgEntry.filePath.substr(0, imgEntry.filePath.lastIndexOf('/') + 1);
+      this.file.removeFile(correctPath, imgEntry.name).then(res => {
+        this.presentToast('File removed.');
+      });
+    });
+  }
+
+  startUpload(imgEntry) {
+    this.file.resolveLocalFilesystemUrl(imgEntry.filePath)
+    .then(entry => {
+      (<FileEntry> entry).file(file => this.readFile(file));
+    }).catch(err => {
+      this.presentToast('Error while reading file.');
+    });
+  }
+
+  readFile(file: any) {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const formData = new FormData();
+      const imgBlob = new Blob([reader.result], {
+        type: file.type
+      });
+      formData.append('file', imgBlob, file.name);
+      this.uploadImageData(formData);
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
+  async uploadImageData(formData: FormData) {
+    const loading = await this.loadingController.create({
+      message: 'Uploading image...'
+    });
+    await loading.present();
+
+    this.http.post('http://localhost:8888/upload.php', formData)
+    .pipe(
+      finalize(() => {
+        loading.dismiss();
+      })
+    )
+    .subscribe(res => {
+      if (res['success']) {
+        this.presentToast('File upload complete.');
+      } else {
+        this.presentToast('File upload failed.');
+      }
+    });
+  }
 }
